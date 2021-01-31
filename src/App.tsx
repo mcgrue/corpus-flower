f960718bbf17a5095943d9cbc9c612620f4c6c96
Ubuntu <ubuntu@ip-172-31-3-197.us-east-2.compute.internal>
2021-01-31 23:05:07 +0000

    Working with good linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -15,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn To Die
         </a>
       </header>
     </div>
